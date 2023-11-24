30d2355ce68144684eb31bd5aac1d8a52bae61a0
shyun290 <shyun@gmail.com>
2023-11-24 10:51:00 +0900

    main

--- server/routes/emailRoutes.js
+++
@@ -1,8 +0,0 @@
-const EmailLogController = require('../controllers/emailLogController');
-const { verifyToken } = require('../middleware/auth');
-const express = require('express');
-const router = new express.Router();
-
-//router.get('/:t_id', EmailLogController.test);
-
-module.exports = router;
